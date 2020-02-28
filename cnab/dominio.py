a6feffee04c37dc3c98363201601e84fb7fcfdac
Lucas Aguiar <lucas.abreu@itbam.org.br>
2020-02-28 10:06:18 -0300

    register_key by cpf_cnpj cerc

--- cnab/dominio.py
+++ cnab/dominio.py
@@ -1,7 +1,9 @@
 
 
-with open("C:\\Users\\lucas.abreu\\Documents\\cnab\\2020-02-26\\20021004.rem", "r") as file:
-    for f in file:
-        print(f.strip())
+# with open("C:\\Users\\lucas.abreu\\Documents\\cnab\\2020-02-26\\20021004.rem", "r") as file:
+#     for f in file:
+#         print(f.strip())
+
+
 
 
